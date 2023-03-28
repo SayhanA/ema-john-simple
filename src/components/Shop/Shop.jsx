3482d75fc75adcdde330025492778afec1feb773
SayhanA <sayhanahmed05@gmail.com>
2023-03-28 13:09:52 +0600

    code added

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -12,11 +13,17 @@ const Shop = () => {
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
-    } ,[])
+    } ,[]);
 
-    const handleAddToCart = (props) => {
-        const newCart = [...cart, props]
-        setCart(newCart)
+    useEffect( () => {
+        const storedCart = getShoppingCart()
+        console.log(storedCart)
+    },[])
+
+    const handleAddToCart = (product) => {
+        const newCart = [...cart, product]
+        setCart(newCart);
+        addToDb(product.id)
     }
     
     return (
