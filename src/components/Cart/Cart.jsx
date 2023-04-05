ccada41b229257e6dcbb3c82ddd28c8090900bb1
SayhanA <sayhanahmed05@gmail.com>
2023-04-05 15:54:37 +0600

    remove button added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,9 +7,16 @@ const Cart = ({cart}) => {
 
     let total = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart){
-        total = total + product.price;
-        totalShipping = totalShipping + product.shipping
+        // if(product.quantity === 0){
+        //     product.quantity = 1 ;
+        // }
+        // product.quantity = product.price * product.quantity;
+        
+        total = total + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping ;
+        quantity = quantity + product.quantity;
     }
     const tax = total*7/100;
     const grandTotal = total + totalShipping + tax
@@ -17,7 +24,7 @@ const Cart = ({cart}) => {
     return (
         <div  className='cart'>
             <h4>Order Summary</h4>
-            <p>Selected items: {cart.length}</p>
+            <p>Selected items: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,42 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Shop from './components/Shop/Shop'
+import Home from './components/Layout/Home'
+import Orders from './components/Orders/Orders'
+import Inventory from './components/Inventory/Inventory'
+import LogIn from './components/LogIn/LogIn'
+import cartProductsLoader from './Loader/cartProductsLoader'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/',
+        element: <Shop></Shop>
+      },
+      {
+        path: '/orders',
+        element: <Orders></Orders>,
+        loader: cartProductsLoader
+      },
+      {
+        path: '/Inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: '/login',
+        element: <LogIn></LogIn>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
